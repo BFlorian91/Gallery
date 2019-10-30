45cc90f8c3288e165248a9a785e5e6ab9179b5e9
Florian <flbeaumo@student.42.fr>
2019-10-30 17:34:15 +0100

    add nav bar + drawpannel + images gallery

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Fb</span>
+        <span class="font-weight-light">DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="/"
+        target="_blank"
+        @onclick="pannelHidden" 
+      >do that: onclick display the pannel component -->
+        <span class="mr-2">Account</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <NavDrawer class="float-right position-absolute"/>
+      <Gallery />
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import HelloWorld from './components/HelloWorld';
+import Gallery from './components/Gallery';
+import NavDrawer from './components/NavDrawer';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+    NavDrawer,
+    Gallery
+  },
+  data: () => ({
+    
+  }), 
+};
+</script>
