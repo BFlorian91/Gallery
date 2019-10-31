6fa7c62de9819f42f944643dbcf27508d334f9c1
florian <b.florian91@icloud.com>
2019-10-31 04:27:52 +0100

    fix name site redirect to the homePage

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,18 @@
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
         <router-link tag="li" to="/">
-        <span>Fb</span>
-        <span class="font-weight-light">DESIGN</span>
+          <button>
+            <span>FB</span>
+            <span class="font-weight-light">DESIGN</span>
+          </button>
         </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-switch :label="`Dark Theme`" @change="$vuetify.theme.dark = !$vuetify.theme.dark" class="mt-6 mr-4"></v-switch>
+      <v-switch
+        :label="`Dark Theme`"
+        @change="$vuetify.theme.dark = !$vuetify.theme.dark"
+        class="mt-6 mr-4"
+      ></v-switch>
       <router-link tag="li" :to="{ name: 'signup' }">
         <v-btn>
           <span class="mr-2">Account</span>
@@ -27,19 +33,18 @@
 
 export default {
   name: "App",
-  components: {
-
-  },
+  components: {},
   data: () => {
-    return {
-
-    };
+    return {};
   }
 };
 </script>
 
 <style scoped>
-  li {
-    list-style-type: none;
-  }
+li {
+  list-style-type: none;
+}
+button:focus {
+  outline: 0;
+}
 </style>
