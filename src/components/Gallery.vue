32913e2f259ab72953848c8e9b2950db3954f4eb
florian <b.florian91@icloud.com>
2019-10-31 04:46:19 +0100

    fix size of gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-row>
-    <v-col cols="12" sm="6" offset-sm="3">
+  <v-row class="mt-4">
+    <v-col cols="12" sm="10" offset-sm="1">
       <v-card>
         <v-container fluid>
           <v-row>
             <v-col v-for="n in 19" :key="n" class="d-flex child-flex" cols="4">
               <v-card flat tile class="d-flex">
                 <v-img
-                  :src="`https://picsum.photos/500/300?image=${n * 2 + 5}`"
+                  :src="`https://picsum.photos/1920/1080?image=${n * 2 + 5}`"
                   :lazy-src="`https://picsum.photos/10/6?image=${n * 2 + 5}`"
                   aspect-ratio="1"
                   class="grey lighten-2"
